4cc65ee583843ab0349c48b7385f48a64c7c25a4
Nynoko228 <krotov2020@gmail.com>
2023-04-28 22:06:54 +0300

    Полный бред

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-import matplotlib.pyplot as plt #для графиков
+import matplotlib.pyplot as plt  # для графиков
 from pylab import mpl
 import math
 # import numpy
@@ -7,49 +7,66 @@ from threading import Thread
 # Аппроксимация полиномиальной кривой с одной переменной
 
 
-import tkinter as tk #для интерфейса
+import tkinter as tk  # для интерфейса
+
 
 def Lagrandj(Xisc, i):
     global x, y
-    # Yisc1 = (4*pow(Xisc, 3)-3*x[1]*pow(Xisc, 2)-3*x[2]*pow(Xisc, 2)-3*x[3]*pow(Xisc, 2)-3*x[4]*pow(Xisc, 2)+2*x[1]*x[2]*Xisc+2*x[1]*x[3]*Xisc+2*x[1]*x[4]*Xisc+2*x[2]*x[3]*Xisc+2*x[2]*x[4]*Xisc+2*x[3]*x[4]*Xisc-x[1]*x[2]*x[3]-x[1]*x[2]*x[4]-x[1]*x[3]*x[4]-x[2]*x[3]*x[4])
+    Yisc1 = (4 * pow(Xisc, 3) - 3 * x[1] * pow(Xisc, 2) - 3 * x[2] * pow(Xisc, 2) - 3 * x[3] * pow(Xisc, 2) - 3 * x[
+        4] * pow(Xisc, 2) + 2 * x[1] * x[2] * Xisc + 2 * x[1] * x[3] * Xisc + 2 * x[1] * x[4] * Xisc + 2 * x[2] * x[
+                 3] * Xisc + 2 * x[2] * x[4] * Xisc + 2 * x[3] * x[4] * Xisc - x[1] * x[2] * x[3] - x[1] * x[2] * x[4] -
+             x[1] * x[3] * x[4] - x[2] * x[3] * x[4])
+    Yisc1 *= y[0]
+    Yisc1 /= ((x[0] - x[1]) * (x[0] - x[2]) * (x[0] - x[3]) * (x[0] - x[4]))
+    Yisc2 = (4 * pow(Xisc, 3) - 3 * x[0] * pow(Xisc, 2) - 3 * x[2] * pow(Xisc, 2) - 3 * x[3] * pow(Xisc, 2) - 3 * x[
+        4] * pow(Xisc, 2) + 2 * x[0] * x[2] * Xisc + 2 * x[0] * x[3] * Xisc + 2 * x[0] * x[4] * Xisc + 2 * x[2] * x[
+                 3] * Xisc + 2 * x[2] * x[4] * Xisc + 2 * x[3] * x[4] * Xisc - x[0] * x[2] * x[3] - x[0] * x[2] * x[4] -
+             x[0] * x[3] * x[4] - x[2] * x[3] * x[4])
+    Yisc2 *= y[1]
+    Yisc2 /= ((x[1] - x[0]) * (x[1] - x[2]) * (x[1] - x[3]) * (x[1] - x[4]))
+    Yisc3 = (4 * pow(Xisc, 3) - 3 * x[0] * pow(Xisc, 2) - 3 * x[1] * pow(Xisc, 2) - 3 * x[3] * pow(Xisc, 2) - 3 * x[
+        4] * pow(Xisc, 2) + 2 * x[0] * x[1] * Xisc + 2 * x[0] * x[3] * Xisc + 2 * x[0] * x[4] * Xisc + 2 * x[1] * x[
+                 3] * Xisc + 2 * x[1] * x[4] * Xisc + 2 * x[3] * x[4] * Xisc - x[0] * x[1] * x[3] - x[0] * x[1] * x[4] -
+             x[0] * x[3] * x[4] - x[1] * x[3] * x[4])
+    Yisc3 *= y[2]
+    Yisc3 /= ((x[2] - x[0]) * (x[2] - x[1]) * (x[2] - x[3]) * (x[2] - x[4]))
+    Yisc4 = (4 * pow(Xisc, 3) - 3 * x[0] * pow(Xisc, 2) - 3 * x[1] * pow(Xisc, 2) - 3 * x[2] * pow(Xisc, 2) - 3 * x[
+        4] * pow(Xisc, 2) + 2 * x[0] * x[1] * Xisc + 2 * x[0] * x[2] * Xisc + 2 * x[0] * x[4] * Xisc + 2 * x[1] * x[
+                 2] * Xisc + 2 * x[1] * x[4] * Xisc + 2 * x[2] * x[4] * Xisc - x[0] * x[1] * x[2] - x[0] * x[1] * x[4] -
+             x[0] * x[2] * x[4] - x[1] * x[2] * x[4])
+    Yisc4 *= y[3]
+    Yisc4 /= ((x[3] - x[0]) * (x[3] - x[1]) * (x[3] - x[2]) * (x[3] - x[4]))
+    Yisc5 = (4 * pow(Xisc, 3) - 3 * x[0] * pow(Xisc, 2) - 3 * x[1] * pow(Xisc, 2) - 3 * x[2] * pow(Xisc, 2) - 3 * x[
+        3] * pow(Xisc, 2) + 2 * x[0] * x[1] * Xisc + 2 * x[0] * x[2] * Xisc + 2 * x[0] * x[3] * Xisc + 2 * x[1] * x[
+                 2] * Xisc + 2 * x[1] * x[3] * Xisc + 2 * x[2] * x[3] * Xisc - x[0] * x[1] * x[2] - x[0] * x[1] * x[3] -
+             x[0] * x[2] * x[3] - x[1] * x[2] * x[3])
+    Yisc5 *= y[4]
+    Yisc5 /= ((x[4] - x[0]) * (x[4] - x[1]) * (x[4] - x[2]) * (x[4] - x[3]))
+    # Yisc1 = (4*pow(Xisc, 3)-3*x[i+1]*pow(Xisc, 2)-3*x[2]*pow(Xisc, 2)-3*x[3]*pow(Xisc, 2)-3*x[4]*pow(Xisc, 2)+2*x[i+1]*x[2]*Xisc+2*x[i+1]*x[3]*Xisc+2*x[i+1]*x[4]*Xisc+2*x[2]*x[3]*Xisc+2*x[2]*x[4]*Xisc+2*x[3]*x[4]*Xisc-x[i+1]*x[2]*x[3]-x[i+1]*x[2]*x[4]-x[i+1]*x[3]*x[4]-x[2]*x[3]*x[4])
     # Yisc1 *= y[0]
-    # Yisc1 /= ((x[0]-x[1])*(x[0]-x[2])*(x[0]-x[3])*(x[0]-x[4]))
+    # Yisc1 /= ((x[0]-x[i+1])*(x[0]-x[2])*(x[0]-x[3])*(x[0]-x[4]))
     # Yisc2 = (4*pow(Xisc, 3)-3*x[0]*pow(Xisc, 2)-3*x[2]*pow(Xisc, 2)-3*x[3]*pow(Xisc, 2)-3*x[4]*pow(Xisc, 2)+2*x[0]*x[2]*Xisc+2*x[0]*x[3]*Xisc+2*x[0]*x[4]*Xisc+2*x[2]*x[3]*Xisc+2*x[2]*x[4]*Xisc+2*x[3]*x[4]*Xisc-x[0]*x[2]*x[3]-x[0]*x[2]*x[4]-x[0]*x[3]*x[4]-x[2]*x[3]*x[4])
     # Yisc2 *= y[1]
-    # Yisc2 /= ((x[1]-x[0])*(x[1]-x[2])*(x[1]-x[3])*(x[1]-x[4]))
-    # Yisc3 = (4*pow(Xisc, 3)-3*x[0]*pow(Xisc, 2)-3*x[1]*pow(Xisc, 2)-3*x[3]*pow(Xisc, 2)-3*x[4]*pow(Xisc, 2)+2*x[0]*x[1]*Xisc+2*x[0]*x[3]*Xisc+2*x[0]*x[4]*Xisc+2*x[1]*x[3]*Xisc+2*x[1]*x[4]*Xisc+2*x[3]*x[4]*Xisc-x[0]*x[1]*x[3]-x[0]*x[1]*x[4]-x[0]*x[3]*x[4]-x[1]*x[3]*x[4])
+    # # Yisc2 /= ((x[i+1]-x[0])*(x[i+1]-x[2])*(x[i+1]-x[3])*(x[i+1]-x[4]))
+    # Yisc3 = (4*pow(Xisc, 3)-3*x[0]*pow(Xisc, 2)-3*x[i+1]*pow(Xisc, 2)-3*x[3]*pow(Xisc, 2)-3*x[4]*pow(Xisc, 2)+2*x[0]*x[i+1]*Xisc+2*x[0]*x[3]*Xisc+2*x[0]*x[4]*Xisc+2*x[i+1]*x[3]*Xisc+2*x[i+1]*x[4]*Xisc+2*x[3]*x[4]*Xisc-x[0]*x[i+1]*x[3]-x[0]*x[i+1]*x[4]-x[0]*x[3]*x[4]-x[i+1]*x[3]*x[4])
     # Yisc3 *= y[2]
-    # Yisc3 /= ((x[2]-x[0])*(x[2]-x[1])*(x[2]-x[3])*(x[2]-x[4]))
-    # Yisc4 = (4*pow(Xisc, 3)-3*x[0]*pow(Xisc, 2)-3*x[1]*pow(Xisc, 2)-3*x[2]*pow(Xisc, 2)-3*x[4]*pow(Xisc, 2)+2*x[0]*x[1]*Xisc+2*x[0]*x[2]*Xisc+2*x[0]*x[4]*Xisc+2*x[1]*x[2]*Xisc+2*x[1]*x[4]*Xisc+2*x[2]*x[4]*Xisc-x[0]*x[1]*x[2]-x[0]*x[1]*x[4]-x[0]*x[2]*x[4]-x[1]*x[2]*x[4])
+    # Yisc3 /= ((x[2]-x[0])*(x[2]-x[i+1])*(x[2]-x[3])*(x[2]-x[4]))
+    # Yisc4 = (4*pow(Xisc, 3)-3*x[0]*pow(Xisc, 2)-3*x[i+1]*pow(Xisc, 2)-3*x[2]*pow(Xisc, 2)-3*x[4]*pow(Xisc, 2)+2*x[0]*x[i+1]*Xisc+2*x[0]*x[2]*Xisc+2*x[0]*x[4]*Xisc+2*x[i+1]*x[2]*Xisc+2*x[i+1]*x[4]*Xisc+2*x[2]*x[4]*Xisc-x[0]*x[i+1]*x[2]-x[0]*x[i+1]*x[4]-x[0]*x[2]*x[4]-x[i+1]*x[2]*x[4])
     # Yisc4 *= y[3]
-    # Yisc4 /= ((x[3]-x[0])*(x[3]-x[1])*(x[3]-x[2])*(x[3]-x[4]))
-    # Yisc5 = (4*pow(Xisc, 3)-3*x[0]*pow(Xisc, 2)-3*x[1]*pow(Xisc, 2)-3*x[2]*pow(Xisc, 2)-3*x[3]*pow(Xisc, 2)+2*x[0]*x[1]*Xisc+2*x[0]*x[2]*Xisc+2*x[0]*x[3]*Xisc+2*x[1]*x[2]*Xisc+2*x[1]*x[3]*Xisc+2*x[2]*x[3]*Xisc-x[0]*x[1]*x[2]-x[0]*x[1]*x[3]-x[0]*x[2]*x[3]-x[1]*x[2]*x[3])
+    # Yisc4 /= ((x[3]-x[0])*(x[3]-x[i+1])*(x[3]-x[2])*(x[3]-x[4]))
+    # Yisc5 = (4*pow(Xisc, 3)-3*x[0]*pow(Xisc, 2)-3*x[i+1]*pow(Xisc, 2)-3*x[2]*pow(Xisc, 2)-3*x[3]*pow(Xisc, 2)+2*x[0]*x[i+1]*Xisc+2*x[0]*x[2]*Xisc+2*x[0]*x[3]*Xisc+2*x[i+1]*x[2]*Xisc+2*x[i+1]*x[3]*Xisc+2*x[2]*x[3]*Xisc-x[0]*x[i+1]*x[2]-x[0]*x[i+1]*x[3]-x[0]*x[2]*x[3]-x[i+1]*x[2]*x[3])
     # Yisc5 *= y[4]
-    # Yisc5 /= ((x[4]-x[0])*(x[4]-x[1])*(x[4]-x[2])*(x[4]-x[3]))
-    Yisc1 = (4*pow(Xisc, 3)-3*x[i+1]*pow(Xisc, 2)-3*x[2]*pow(Xisc, 2)-3*x[3]*pow(Xisc, 2)-3*x[4]*pow(Xisc, 2)+2*x[1]*x[2]*Xisc+2*x[1]*x[3]*Xisc+2*x[1]*x[4]*Xisc+2*x[2]*x[3]*Xisc+2*x[2]*x[4]*Xisc+2*x[3]*x[4]*Xisc-x[1]*x[2]*x[3]-x[1]*x[2]*x[4]-x[1]*x[3]*x[4]-x[2]*x[3]*x[4])
-    Yisc1 *= y[0]
-    Yisc1 /= ((x[0]-x[1])*(x[0]-x[2])*(x[0]-x[3])*(x[0]-x[4]))
-    Yisc2 = (4*pow(Xisc, 3)-3*x[0]*pow(Xisc, 2)-3*x[2]*pow(Xisc, 2)-3*x[3]*pow(Xisc, 2)-3*x[4]*pow(Xisc, 2)+2*x[0]*x[2]*Xisc+2*x[0]*x[3]*Xisc+2*x[0]*x[4]*Xisc+2*x[2]*x[3]*Xisc+2*x[2]*x[4]*Xisc+2*x[3]*x[4]*Xisc-x[0]*x[2]*x[3]-x[0]*x[2]*x[4]-x[0]*x[3]*x[4]-x[2]*x[3]*x[4])
-    Yisc2 *= y[1]
-    Yisc2 /= ((x[1]-x[0])*(x[1]-x[2])*(x[1]-x[3])*(x[1]-x[4]))
-    Yisc3 = (4*pow(Xisc, 3)-3*x[0]*pow(Xisc, 2)-3*x[1]*pow(Xisc, 2)-3*x[3]*pow(Xisc, 2)-3*x[4]*pow(Xisc, 2)+2*x[0]*x[1]*Xisc+2*x[0]*x[3]*Xisc+2*x[0]*x[4]*Xisc+2*x[1]*x[3]*Xisc+2*x[1]*x[4]*Xisc+2*x[3]*x[4]*Xisc-x[0]*x[1]*x[3]-x[0]*x[1]*x[4]-x[0]*x[3]*x[4]-x[1]*x[3]*x[4])
-    Yisc3 *= y[2]
-    Yisc3 /= ((x[2]-x[0])*(x[2]-x[1])*(x[2]-x[3])*(x[2]-x[4]))
-    Yisc4 = (4*pow(Xisc, 3)-3*x[0]*pow(Xisc, 2)-3*x[1]*pow(Xisc, 2)-3*x[2]*pow(Xisc, 2)-3*x[4]*pow(Xisc, 2)+2*x[0]*x[1]*Xisc+2*x[0]*x[2]*Xisc+2*x[0]*x[4]*Xisc+2*x[1]*x[2]*Xisc+2*x[1]*x[4]*Xisc+2*x[2]*x[4]*Xisc-x[0]*x[1]*x[2]-x[0]*x[1]*x[4]-x[0]*x[2]*x[4]-x[1]*x[2]*x[4])
-    Yisc4 *= y[3]
-    Yisc4 /= ((x[3]-x[0])*(x[3]-x[1])*(x[3]-x[2])*(x[3]-x[4]))
-    Yisc5 = (4*pow(Xisc, 3)-3*x[0]*pow(Xisc, 2)-3*x[1]*pow(Xisc, 2)-3*x[2]*pow(Xisc, 2)-3*x[3]*pow(Xisc, 2)+2*x[0]*x[1]*Xisc+2*x[0]*x[2]*Xisc+2*x[0]*x[3]*Xisc+2*x[1]*x[2]*Xisc+2*x[1]*x[3]*Xisc+2*x[2]*x[3]*Xisc-x[0]*x[1]*x[2]-x[0]*x[1]*x[3]-x[0]*x[2]*x[3]-x[1]*x[2]*x[3])
-    Yisc5 *= y[4]
-    Yisc5 /= ((x[4]-x[0])*(x[4]-x[1])*(x[4]-x[2])*(x[4]-x[3]))
-    return Yisc1+Yisc2+Yisc3+Yisc4+Yisc5
+    # Yisc5 /= ((x[4]-x[0])*(x[4]-x[i+1])*(x[4]-x[2])*(x[4]-x[3]))
+    return Yisc1 + Yisc2 + Yisc3 + Yisc4 + Yisc5
+
 
 def Test():
     global x, y
     k = 12
     # k = 6
-    h = float(x[1]-x[0])
+    h = float(x[1] - x[0])
 
-    Yi = [] # Пятые разности
+    Yi = []  # Пятые разности
     for i in range(len(x)):
         Yi.append(Lagrandj(x[i], i))
     # Yi.append(1/(12*round(h,2))*(-3*round(Lagrandj(x[4]),2)+16*round(Lagrandj(x[3]),2)-36*round(Lagrandj(x[2]),2)+48*round(Lagrandj(x[1]),2)-25*round(Lagrandj(x[0]),2)))
@@ -67,9 +84,11 @@ def Test():
     # h = x[-1] - x[-2]
     # Yi.append(1 / (12 * round(h,2)) * (3 * round(Lagrandj(x[-5]),2) - 16 * round(Lagrandj(x[-4]),2) + 36 * round(Lagrandj(x[-3]),2) - 48 * round(Lagrandj(x[-2]),2) + 25 * round(Lagrandj(x[-1]),2)))
 
-    Yj = [] # Третьи разности
+    Yj = []  # Третьи разности
     for i in range(len(x)):
-        Yj.append(((2*x[i]-x[1]-x[2])*y[0]/((x[0]-x[1])*(x[0])-x[2]))+((2*x[i]-x[0]-x[2])*y[1]/((x[1]-x[0])*(x[1])-x[2]))+((2*x[i]-x[0]-x[1])*y[2]/((x[2]-x[0])*(x[2])-x[1])))
+        Yj.append(((2 * x[i] - x[1] - x[2]) * y[0]) / ((x[0] - x[1]) * (x[0] - x[2])) + (
+                ((2 * x[i] - x[0] - x[2]) * y[1]) / ((x[1] - x[0]) * (x[1] - x[2])) + (
+                ((2 * x[i] - x[0] - x[1]) * y[2]) / ((x[2] - x[0]) * (x[2] - x[1])))))
 
     # h = x[1] - x[0]
     # Yj.append(1/(2*round(h,2))*(-round(Lagrandj(x[2]),2)+4*round(Lagrandj(x[1]),2)-3*round(Lagrandj(x[0]),2)))
@@ -83,6 +102,8 @@ def Test():
     # Yj.append(1/(2*round(h,2))*(round(Lagrandj(x[-3]),2)-4*round(Lagrandj(x[-2]),2)+3*round(Lagrandj(x[-1]),2)))
     draw(x, y, Yj, Yi)
     # thread.start()
+
+
 def formula():
     global x, y
     fin = []
@@ -90,13 +111,18 @@ def formula():
     # k = 6
     # h = float(x[1]-x[0])
     # h = float((x[4] - x[0]) / 4)
-    Yi = [] # Пятые разности
+    Yi = []  # Пятые разности
     fin += ["Пятые разности: "]
-    fin += [r"$P^{'}(x)=\frac{(4x^{3}-3x_{1}x^{2}-3x_{2}x^{2}-3x_{3}x^{2}-3x_{4}x^{2}+2x_{1}x_{2}x+2x_{1}x_{3}x+2x_{1}x_{4}x+2x_{2}x_{3}x+2x_{2}x_{4}x+2x_{3}x_{4}x-x_{1}x_{2}x_{3}-x_{1}x_{2}x_{4}-x_{1}x_{3}x_{4}-x_{2}x_{3}x_{4})y_{0}}{(x_{0}-x_{1})(x_{0}-x_{2})(x_{0}-x_{3})(x_{0}-x_{4})}$"]
-    fin += [r"$+\frac{(4x^{3}-3x_{0}x^{2}-3x_{2}x^{2}-3x_{3}x^{2}-3x_{4}x^{2}+2x_{0}x_{2}x+2x_{0}x_{3}x+2x_{0}x_{4}x+2x_{2}x_{3}x+2x_{2}x_{4}x+2x_{3}x_{4}x-x_{0}x_{2}x_{3}-x_{0}x_{2}x_{4}-x_{0}x_{3}x_{4}-x_{2}x_{3}x_{4})y_{1}}{(x_{1}-x_{0})(x_{1}-x_{2})(x_{1}-x_{3})(x_{1}-x_{4})}$"]
-    fin += [r"$+\frac{(4x^{3}-3x_{0}x^{2}-3x_{1}x^{2}-3x_{3}x^{2}-3x_{4}x^{2}+2x_{0}x_{1}x+2x_{0}x_{3}x+2x_{0}x_{4}x+2x_{1}x_{3}x+2x_{1}x_{4}x+2x_{3}x_{4}x-x_{0}x_{1}x_{3}-x_{0}x_{1}x_{4}-x_{0}x_{3}x_{4}-x_{1}x_{3}x_{4})y_{2}}{(x_{2}-x_{0})(x_{2}-x_{1})(x_{2}-x_{3})(x_{2}-x_{4})}$"]
-    fin += [r"$+\frac{(4x^{3}-3x_{0}x^{2}-3x_{1}x^{2}-3x_{2}x^{2}-3x_{4}x^{2}+2x_{0}x_{1}x+2x_{0}x_{2}x+2x_{0}x_{4}x+2x_{1}x_{2}x+2x_{1}x_{4}x+2x_{2}x_{4}x-x_{0}x_{1}x_{2}-x_{0}x_{1}x_{4}-x_{0}x_{2}x_{4}-x_{1}x_{2}x_{4})y_{3}}{(x_{3}-x_{0})(x_{3}-x_{1})(x_{3}-x_{2})(x_{3}-x_{4})}$"]
-    fin += [r"$+\frac{(4x^{3}-3x_{0}x^{2}-3x_{1}x^{2}-3x_{2}x^{2}-3x_{3}x^{2}+2x_{0}x_{1}x+2x_{0}x_{2}x+2x_{0}x_{3}x+2x_{1}x_{2}x+2x_{1}x_{3}x+2x_{2}x_{3}x-x_{0}x_{1}x_{2}-x_{0}x_{1}x_{3}-x_{0}x_{2}x_{3}-x_{1}x_{2}x_{3})y_{4}}{(x_{4}-x_{0})(x_{4}-x_{1})(x_{4}-x_{2})(x_{4}-x_{3})}$"]
+    fin += [
+        r"$P^{'}(x)=\frac{(4x^{3}-3x_{1}x^{2}-3x_{2}x^{2}-3x_{3}x^{2}-3x_{4}x^{2}+2x_{1}x_{2}x+2x_{1}x_{3}x+2x_{1}x_{4}x+2x_{2}x_{3}x+2x_{2}x_{4}x+2x_{3}x_{4}x-x_{1}x_{2}x_{3}-x_{1}x_{2}x_{4}-x_{1}x_{3}x_{4}-x_{2}x_{3}x_{4})y_{0}}{(x_{0}-x_{1})(x_{0}-x_{2})(x_{0}-x_{3})(x_{0}-x_{4})}$"]
+    fin += [
+        r"$+\frac{(4x^{3}-3x_{0}x^{2}-3x_{2}x^{2}-3x_{3}x^{2}-3x_{4}x^{2}+2x_{0}x_{2}x+2x_{0}x_{3}x+2x_{0}x_{4}x+2x_{2}x_{3}x+2x_{2}x_{4}x+2x_{3}x_{4}x-x_{0}x_{2}x_{3}-x_{0}x_{2}x_{4}-x_{0}x_{3}x_{4}-x_{2}x_{3}x_{4})y_{1}}{(x_{1}-x_{0})(x_{1}-x_{2})(x_{1}-x_{3})(x_{1}-x_{4})}$"]
+    fin += [
+        r"$+\frac{(4x^{3}-3x_{0}x^{2}-3x_{1}x^{2}-3x_{3}x^{2}-3x_{4}x^{2}+2x_{0}x_{1}x+2x_{0}x_{3}x+2x_{0}x_{4}x+2x_{1}x_{3}x+2x_{1}x_{4}x+2x_{3}x_{4}x-x_{0}x_{1}x_{3}-x_{0}x_{1}x_{4}-x_{0}x_{3}x_{4}-x_{1}x_{3}x_{4})y_{2}}{(x_{2}-x_{0})(x_{2}-x_{1})(x_{2}-x_{3})(x_{2}-x_{4})}$"]
+    fin += [
+        r"$+\frac{(4x^{3}-3x_{0}x^{2}-3x_{1}x^{2}-3x_{2}x^{2}-3x_{4}x^{2}+2x_{0}x_{1}x+2x_{0}x_{2}x+2x_{0}x_{4}x+2x_{1}x_{2}x+2x_{1}x_{4}x+2x_{2}x_{4}x-x_{0}x_{1}x_{2}-x_{0}x_{1}x_{4}-x_{0}x_{2}x_{4}-x_{1}x_{2}x_{4})y_{3}}{(x_{3}-x_{0})(x_{3}-x_{1})(x_{3}-x_{2})(x_{3}-x_{4})}$"]
+    fin += [
+        r"$+\frac{(4x^{3}-3x_{0}x^{2}-3x_{1}x^{2}-3x_{2}x^{2}-3x_{3}x^{2}+2x_{0}x_{1}x+2x_{0}x_{2}x+2x_{0}x_{3}x+2x_{1}x_{2}x+2x_{1}x_{3}x+2x_{2}x_{3}x-x_{0}x_{1}x_{2}-x_{0}x_{1}x_{3}-x_{0}x_{2}x_{3}-x_{1}x_{2}x_{3})y_{4}}{(x_{4}-x_{0})(x_{4}-x_{1})(x_{4}-x_{2})(x_{4}-x_{3})}$"]
 
     # for i in range(len(x)):
     #     Yi.append(4*x[i])
@@ -125,9 +151,10 @@ def formula():
     # fin += [rf"$y_{{{11}}}^'=\frac{{3({round(Lagrandj(x[-5]),2)})-16({round(Lagrandj(x[-4]),2)})+36({round(Lagrandj(x[-3]),2)})-48({round(Lagrandj(x[-2]),2)})+25({round(Lagrandj(x[-1]),2)})}}{{12({(round(h,2))})}}$"]
     # Yi.append(1 / (12 * h) * (3 * Lagrandj(x[k - 1] - 4 * h) - 16 * Lagrandj(x[k - 1] - 3 * h) + 36 * Lagrandj(x[k - 1] - 2 * h) - 48 * Lagrandj(x[k - 1] - 1 * h) + 25 * Lagrandj(x[k - 1])))
 
-    Yj = [] # Третьи разности
+    Yj = []  # Третьи разности
     fin += ["Третьи разности: "]
-    fin += [r"$P^{'}(x)=\frac{(2x-x_{1}-x_{2})y_{0}}{(x_{0}-x_{1})(x_{0}-x_{2})}+\frac{(2x-x_{0}-x_{2})y_{1}}{(x_{1}-x_{0})(x_{1}-x_{2})}+\frac{(2x-x_{0}-x_{1})y_{2}}{(x_{2}-x_{0})(x_{2}-x_{1})}$"]
+    fin += [
+        r"$P^{'}(x)=\frac{(2x-x_{1}-x_{2})y_{0}}{(x_{0}-x_{1})(x_{0}-x_{2})}+\frac{(2x-x_{0}-x_{2})y_{1}}{(x_{1}-x_{0})(x_{1}-x_{2})}+\frac{(2x-x_{0}-x_{1})y_{2}}{(x_{2}-x_{0})(x_{2}-x_{1})}$"]
     # h = x[1] - x[0]
     # fin += [r"Левые: $y_{0}^'=\frac{{-y_{2}+4y_{1}-3y_{0}}}{{2(x_{1}-x_{0})}}$"]
     # Yj.append(1/(2*(x[1]-x[0]))*(-Lagrandj(x[0]+2*h)+4*Lagrandj(x[0]+1*h)-3*Lagrandj(x[0])))
@@ -146,6 +173,7 @@ def formula():
     root = tk.Toplevel()
     app = Application(fin, master=root)
     app.mainloop()
+
 
 # Функция draw рисует наши кривые на координатной плоскости
 def draw(xL, yL, y3, y5):
